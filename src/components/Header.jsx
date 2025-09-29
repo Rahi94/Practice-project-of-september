6126f84819ffb463526132ad0788f790c822f24f
tasib94 <rahi84974@gmail.com>
2025-09-29 23:38:29 +0600

    implemented date fns to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from "../assets/logo.png";
+import { format } from 'date-fns';
 const Header = () => {
     return (
         <div className='flex justify-center flex-col items-center gap-3'>
             <img className='w-[400px]' src={logo} alt="" />
             <p>Lorem ipsum, dolor sit</p>
-            <button className='btn btn-secondary'>Click me</button>
-
+            <p className='text-accent text-semibold'>{format(new Date(),"EEEE, MMMM do, yyyy")}</p>
         </div>
     );
 };
